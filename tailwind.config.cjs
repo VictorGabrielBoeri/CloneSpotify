bf041e30f8e324c060996cba85dc73d9c949d9ab
victor3381 <victor.gabriel@adaptweb.com.br>
2025-05-14 13:21:16 -0300

    fix: corrigir configurações para deploy no Vercel

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'spotify-black': '#121212',
+        'spotify-gray': '#181818',
+        'spotify-light-gray': '#b3b3b3',
+        'spotify-green': '#1DB954'
+      }
+    }
+  },
+  plugins: []
+}
